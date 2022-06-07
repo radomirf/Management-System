e1ac32ca2ce77afe90aad7b82a98402d755449a6
Radefe <Radefe40@gmail.com>
2022-06-07 09:04:37 +0300

    Working With DB without Linq

--- WPF ZooManager/MainWindow.xaml.cs
+++ WPF ZooManager/MainWindow.xaml.cs
@@ -31,9 +31,10 @@ namespace WPF_ZooManager
             string connectionString = ConfigurationManager.ConnectionStrings["WPF_ZooManager.Properties.Settings.RadefeConnectionString"].ConnectionString;
             sqlConnection = new SqlConnection(connectionString);
             ShowZoos();
+            ShowAllAnimals();
         }
-      
-       private void ShowZoos()
+
+        private void ShowZoos()
         {
             try
             {
@@ -47,7 +48,8 @@ namespace WPF_ZooManager
                     listZoos.SelectedValuePath = "Id";
                     listZoos.ItemsSource = zooTable.DefaultView;
                 }
-            }catch(Exception e)
+            }
+            catch (Exception e)
             {
                 MessageBox.Show(e.Message);
             }
@@ -71,12 +73,193 @@ namespace WPF_ZooManager
             }
             catch (Exception e)
             {
+                // MessageBox.Show(e.Message);
+            }
+        }
+        private void ShowAllAnimals()
+        {
+            try
+            {
+                string query = "select * from Animal";
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+                using (sqlDataAdapter)
+                {
+                    DataTable animalTable = new DataTable();
+                    sqlDataAdapter.Fill(animalTable);
+                    Animals.DisplayMemberPath = "Name";
+                    Animals.SelectedValuePath = "Id";
+                    Animals.ItemsSource = animalTable.DefaultView;
+                }
+            }
+            catch (Exception e)
+            {
                 MessageBox.Show(e.Message);
             }
         }
         private void listZoos_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            ShowSelectedInTextBox("select location from Zoo where Id = @ZooId", "@ZooId", listZoos, "Location");
+
             ShowAnimals();
+        }
+        private void listAnimals_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ShowSelectedInTextBox("select name from Animal where Id = @AnimalId", "@AnimalId", Animals, "Name");
+        }
+        private void DeleteZoo(object sender, RoutedEventArgs e)
+        {
+            DBChange("delete from Zoo where Id=@ZooId", "@ZooId", listZoos);
+
+        }
+        private void AddZoo_OnClick(object sender, RoutedEventArgs e)
+        {
+            DBChange("insert into Zoo values (@Location)", "@Location");
+        }
+
+        private void AddAnimalToZoo_Click(object sender, RoutedEventArgs e)
+        {
+            DBChange("insert into ZooAnimal values (@ZooId, @AnimalId)", "@AnimalId", "@ZooId", Animals, listZoos);
+        }
+        private void DeleteAnimal_Click(object sender, RoutedEventArgs e)
+        {
+            DBChange("delete from Animal where Id=@AnimalId", "@AnimalId", Animals);
+
+        }
+        private void RemoveFromZoo_Click(object sender, RoutedEventArgs e)
+        {
+            DBChange("delete from ZooAnimal where ZooId=@ZooId AND AnimalId=@AnimalId", "@AnimalId", "@ZooId", listAnimals, listZoos);
+        }
+        private void AddAnimal_Click(object sender, RoutedEventArgs e)
+        {
+            DBChange("insert into Animal values (@Name)", "@Name");
+        }
+        private void DBChange(string s, string value, ListBox box)
+        {
+            try
+            {
+                string query = s;
+                SqlCommand sqlcommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlcommand.Parameters.AddWithValue(value, box.SelectedValue);
+                sqlcommand.ExecuteScalar();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                sqlConnection.Close();
+                ShowAllAnimals();
+                ShowAnimals();
+                ShowZoos();
+
+            }
+        }
+        private void DBChange(string s, string value)
+        {
+            try
+            {
+                string query = s;
+                SqlCommand sqlcommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlcommand.Parameters.AddWithValue(value, MyTextBox.Text);
+                sqlcommand.ExecuteScalar();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                sqlConnection.Close();
+                ShowAllAnimals();
+                ShowAnimals();
+                ShowZoos();
+            }
+        }
+        private void DBChange(string s, string value1, string value2, ListBox one, ListBox two)
+        {
+            try
+            {
+                string query = s;
+                SqlCommand sqlcommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlcommand.Parameters.AddWithValue(value1, one.SelectedValue);
+                sqlcommand.Parameters.AddWithValue(value2, two.SelectedValue);
+                sqlcommand.ExecuteScalar();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                sqlConnection.Close();
+                ShowAnimals();
+
+            }
+        }
+        private void DBChange(string s, string value1, string value2, ListBox list)
+        {
+            try
+            {
+                string query = s;
+                SqlCommand sqlcommand = new SqlCommand(query, sqlConnection);
+                sqlConnection.Open();
+                sqlcommand.Parameters.AddWithValue(value1, list.SelectedValue);
+                sqlcommand.Parameters.AddWithValue(value2, MyTextBox.Text);
+                sqlcommand.ExecuteScalar();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                sqlConnection.Close();
+                ShowAnimals();
+                ShowZoos();
+                ShowAllAnimals();
+
+            }
+        }
+        private void UpdateZoo_Click(object sender, RoutedEventArgs e)
+        {
+            DBChange("update Zoo Set Location = @Location where Id = @ZooId", "@ZooId", "@Location", listZoos);
+        }
+
+        private void UpdateAnimal_Click(object sender, RoutedEventArgs e)
+        {
+            DBChange("update Animal Set Name = @Name where Id = @AnimalId", "@AnimalId", "@Name", Animals);
+        }
+        private void ShowSelectedInTextBox(string s, string value, ListBox list, string selection)
+        {
+            try
+            {
+                string query = s;
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                using (sqlDataAdapter)
+                {
+                    sqlCommand.Parameters.AddWithValue(value, list.SelectedValue);
+                    DataTable Table = new DataTable();
+                    sqlDataAdapter.Fill(Table);
+                    MyTextBox.Text = Table.Rows[0][selection].ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                // MessageBox.Show(e.Message);
+            }
         }
     }
 }
